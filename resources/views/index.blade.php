ad9991b8ee0181446b5705d411f0f7d9a118c7de
John Doe <r0706426@student.thomasmore.be>
2021-10-31 15:49:07 +0100

    base navigation and styling done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,34 +7,50 @@
     
     <title>Home</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz:wght@200;400;600&display=swap" rel="stylesheet">
 </head>
 <body>
-    <!-- <h1>Welcome to our homepage</h1>
 
-    @foreach ($nfts as $nft)
+    {{-- @foreach ($nfts as $nft)
     <div>
         <a href="/nfts/{{ $nft->id }}">{{ $nft->name }}</a>
     </div>
-    @endforeach -->
+    @endforeach --}}
 
-    <nav>
-        <div>
-            <h1>test</h1>
-            <img src="{{ asset('./../images/logo.png') }}" alt="test">
-            <div>
-                <input type="text"  placeholder="What are you looking for?">
-                <button type="submit">
-                <img src="{{ asset('./../images/search.png') }}" alt="search">
-            </button>
+    <nav class="bg-gray font-headers text-base text-xl">
+        <div class="max-w-8xl mx-auto border border-red-400 h-20 pt-2">
+            <div class="flex justify-between pr-20 pl-20 items-center">
+                <div class="flex space-x-32 items-center">
+                    <!-- logo -->
+                    <div class="flex items-center">
+                        <a href="#"> <img  class="w-14 h-14" src="{{ asset('./../images/logo.png') }}" alt="logo"> </a>
+                        <span>Sea hackers</span>
+                    </div>
+                    <!-- search -->
+                    <div class="border-2 border-mainblue rounded pl-5 pr-5">
+                    <input type="text"  class="outline-none" placeholder="Search..." size="40">
+                    <button type="submit" class="">
+                        <img class="w-4" src="{{ asset('./../images/search.png') }}" alt="search">
+                    </button>
+                    </div>
+                </div>
+                    <!-- navigation -->
+                    <div class="flex space-x-5">
+                        <a href="">Home</a>
+                        <a href="">Collections</a>
+                        <a href="">Market</a>
+                        @if(Auth::check())
+                        <a href=""> <img class="h-6" src="{{ asset('./../images/profile.png') }}" alt="profile"></a>
+                        @else
+                        <a href="">login/register</a>
+                        @endif
+                        
+                    </div>
+
+
             </div>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Collections</a></li>
-                <li><a href="">Market</a></li>
-                <li><a href="">login/register</a></li>
-            </ul>
-            
-            
         </div>
 
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainblue: '#BBD3D7',
+      },
+      fontFamily:{
+        headers: 'Yanone Kaffeesatz, sans-serif',
+      }
+    },
   },
   variants: {
     extend: {},
