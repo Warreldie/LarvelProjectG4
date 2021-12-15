159f1ce6a1816dd51323643b44b3a2a4f59cecd7
John Doe <r0706426@student.thomasmore.be>
2021-12-15 16:27:19 +0100

    homepage fixed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,36 +2,48 @@
 
 @section('content')
 
-        </div>
-        @endif
-        <!-- left side -->
-        <div class="flex">
+<x-navigation/>
+<section class="bg-home bg-cover pb-40">
+@if(Session::has('message'))
+  <div class="bg-green-400 text-center rounded text-white mb-5">
+    <p>
+    {{Session::get('message')}}
+    </p>
 
-            <div class="mt-40 ml-32">
-                <h1 class="font-headers font-semibold text-6xl text-h1">
-                    Together we can <br>
-                    help our seas recover
-                </h1>
-                <h3 class="text-h3 font-headers font-regular text-3xl mt-4">
-                    collect and trade
-                </h3>
-                <div class="mt-10 font-headers font-extralight text-2xl flex justify-around">
-                    <a class="rounded bg-white  py-0.5 px-8 text-regular shadow-md" href="">More info</a>
-                    <a class="rounded bg-white  py-0.5 px-8 text-regular shadow-md" href="">Register</a>
-                </div>
-            </div>
+    @php
+    Session::forget('message');
+    @endphp
+    
+  </div>
+  @endif
+    <!-- left side -->
+    <div class="flex">
+
+        <div class="mt-40 ml-32">
+        <h1 class="font-headers font-semibold text-6xl text-h1">
+            Together we can <br>
+            help our seas recover
+        </h1>
+        <h3 class="text-h3 font-headers font-regular text-3xl mt-4">
+            collect and trade
+        </h3>
+        <div class="mt-10 font-headers font-extralight text-2xl flex justify-around">
+            <a class="rounded bg-white  py-0.5 px-8 text-regular shadow-md" href="">More info</a>
+            <a class="rounded bg-white  py-0.5 px-8 text-regular shadow-md" href="">Register</a>
+        </div>
+        </div>
 
 
-        </div>
-        <!-- right side -->
-        <div class="flex">
+    </div>
+    <!-- right side -->
+    <div class="flex">
 
-        </div>
-    </section>
-    <section class="flex justify-center">
-        <div class="mt-10">
-            <h2 class="font-headers font-semibold text-4xl">Featured</h2>
-        </div>
+    </div>
+</section>
+<section class="flex justify-center">
+    <div class="mt-10">
+        <h2 class="font-headers font-semibold text-4xl">Featured</h2>
+    </div>
 
 </section>
 
